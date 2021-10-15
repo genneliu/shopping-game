bbbad70b6535c6ba7ddb11869333017216296bb8
Genne Liu <genneliu@gmail.com>
2021-10-15 14:45:56 -0700

    second module

--- js/shoppinggame.js
+++ js/shoppinggame.js
@@ -1,8 +1,15 @@
 let gameComplete = false;
 // Define the three constants here
+const name = "unknown";
+const score = 0;
+const items = 0;
 
 // Define the player object here
-
+let player = { 
+getCurrentScore(score),
+addPoints()
+deductPoints()
+}
 // Define the Product class - write the Constructor function for Product class here
 
 // Complete the dateDiff function
