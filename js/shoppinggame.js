2e524ff427cb7b01d401130e0d1d52b9430f2791
Genne Liu <genneliu@gmail.com>
2021-10-17 12:12:28 -0700

    module 5

--- js/shoppinggame.js
+++ js/shoppinggame.js
@@ -209,10 +209,16 @@ function loadMasterData() {
 }
 
 // Complete this function
-const findProductById = (id) => {};
+const findProductById = (id) => {
+    return function (product) {
+        return product.id === id;
+    }
+ };
 
 // Complete this function
-const generateProductId = () => {};
+const generateProductId = () => {
+    return (Math.floor(Math.random()) * 20 + 1)
+};
 
 
 const getProduct = (prodList, pId) => {
@@ -221,7 +227,9 @@ const getProduct = (prodList, pId) => {
 
 
 // Complete this function
-const calculateBill = (prod, tBill) => {};
+const calculateBill = (prod, tBill) => {
+    return tBill + prod.price
+};
 
 const findPointsToBill = (roundedTotal) => {
     if (roundedTotal > 10 && roundedTotal <= 100) {
@@ -243,12 +251,26 @@ const findPointsToBill = (roundedTotal) => {
 
 
 // Complete this function
-const findPointsForExpDate = (prod) => {};
+const findPointsForExpDate = (prod) => {
+    if(prod.daysToExpire < 30) {
+        return 10;
+    } else {
+        return 0;
+    }
+};
 
 
 const calculatePoints = (prod, tBill) => {
     let pointsToBill = findPointsToBill(Math.round(tBill));
     let pointsForExpDate = findPointsForExpDate(prod);
+    player.score = player.score + pointsToBill + pointsForExpDate;
+    if (prod instanceof MagicProduct) {
+        if (prod.isBonus) {
+            player.addPoints(prod.points)
+        } else {
+            player.deductPoints(prod.points)
+        }
+    }
 };
 
 // Complete this function
